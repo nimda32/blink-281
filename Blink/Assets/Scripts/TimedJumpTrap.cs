36b442bf46f412012852a66d24e927629e18f026
Thien <thientran94@gmail.com>
2016-12-09 19:01:25 -0800

    new project folder

---
+++ Blink/Assets/Scripts/TimedJumpTrap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedJumpTrap : MonoBehaviour {
+
+	public GameObject wall;
+
+	void Start () {
+		StartCoroutine (toggleDeathWall());
+	}
+		
+	bool isWallActive = false;
+
+	IEnumerator toggleDeathWall() {
+		//insures each trap starts at different times
+		int wait_time = Random.Range (0, 3);
+		yield return new WaitForSeconds (wait_time);
+
+		//toggle the wall of death
+		for(;;) {
+
+			wall.SetActive( isWallActive );
+			isWallActive = !isWallActive;
+			//Debug.Log ("death is" + isWallActive);
+			yield return new WaitForSeconds(1f);
+
+		}
+	}
+}
---
+++ Blink/Assets/Scripts/Trap.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+	}
+}
